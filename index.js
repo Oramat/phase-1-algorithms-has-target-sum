17c866c618ac56d0e769f439a3d181a2e43e4104
Maxwel <maxweloramat@gmail.com>
2022-06-15 09:25:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
+}
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+function findSock(object) {
+  if (object.sock) return "sock";
+}
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -29,6 +32,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
